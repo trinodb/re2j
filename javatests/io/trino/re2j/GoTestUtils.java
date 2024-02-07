8ff7fb206c6e049bd69af8734f29d819eb99db3b
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-02-07 13:51:53 +0100

    Move Trino RE2J fork to a new package
    There are situations where downstream dependencies require com.google.re2j:1.7 and we pull in both dependencies due to a dependence on Trino parts.
    
    These conflicts cannot be resolved if both dependencies use the same package.

---
+++ javatests/io/trino/re2j/GoTestUtils.java
@@ -0,0 +1,34 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+
+package io.trino.re2j;
+
+import java.io.UnsupportedEncodingException;
+
+// Utilities to make JUnit act a little more like Go's "testing" package.
+class GoTestUtils {
+  // Other utilities:
+
+  protected static int len(Object[] array) {
+    return array == null ? 0 : array.length;
+  }
+  protected static int len(int[] array) {
+    return array == null ? 0 : array.length;
+  }
+
+  protected static byte[] utf8(String s) {
+    try {
+      return s.getBytes("UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new IllegalStateException("can't happen");
+    }
+  }
+
+  // Beware: logically this operation can fail, but Java doesn't detect it.
+  protected static String fromUTF8(byte[] b) {
+    try {
+      return new String(b, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new IllegalStateException("can't happen");
+    }
+  }
+}
---
+++ javatests/io/trino/re2j/RE2Test.java
@@ -0,0 +1,65 @@
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Original Go source here:
+// http://code.google.com/p/go/source/browse/src/pkg/regexp/syntax/all_test.go
+
+package io.trino.re2j;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static io.airlift.slice.Slices.utf8Slice;
+import static io.trino.re2j.RE2.Anchor.ANCHOR_BOTH;
+import static io.trino.re2j.RE2.Anchor.UNANCHORED;
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Tests of RE2 API.
+ */
+@RunWith(Enclosed.class)
+public class RE2Test {
+
+  public static class DFA extends RE2TestBase {
+    public DFA() {
+      super(RUN_WITH_DFA);
+    }
+  }
+
+  public static class NFA extends RE2TestBase {
+    public NFA() {
+      super(RUN_WITH_NFA);
+    }
+  }
+
+  public static abstract class RE2TestBase extends OptionsTest {
+
+    protected RE2TestBase(Options options) {
+      super(options);
+    }
+
+    @Test
+    public void testFullMatch() {
+      assertEquals(true, RE2.compile("ab+c", options).match(
+          utf8Slice("abbbbbc"), 0, ANCHOR_BOTH, null, 0));
+      assertEquals(false, RE2.compile("ab+c", options).match(
+          utf8Slice("xabbbbbc"), 0, ANCHOR_BOTH, null, 0));
+    }
+
+    @Test
+    public void testFindEnd() {
+      RE2 r = RE2.compile("abc.*def", options);
+      assertEquals(true, r.match(utf8Slice("yyyabcxxxdefzzz"),
+          0, UNANCHORED, null, 0));
+      assertEquals(true, r.match(utf8Slice("yyyabcxxxdefzzz"),
+          3, UNANCHORED, null, 0));
+      assertEquals(false, r.match(utf8Slice("yyyabcxxxdefzzz"),
+          4, UNANCHORED, null, 0));
+      assertEquals(false, r.match(utf8Slice("abcxxxde"),
+          3, UNANCHORED, null, 0));
+    }
+  }
+}
